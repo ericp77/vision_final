56d2a7097efc1fda0295649b53279ef4d2944adc
Seung <spaik@columbia.edu>
2023-04-21 13:34:19 -0400

    Add logger

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import argparse
+import os.path
 
 import torch
 from lightning import Trainer
 
 from data.dataModule import DataModule
 from model.wrapperModel import WrapperModel
+
+from lightning.pytorch.loggers import WandbLogger
+
+def get_model_args():
+    model_parser = argparse.ArgumentParser()
+    model_parser.add_argument("--lr", type=float, default=3e-5)
+
+    return model_parser.parse_args()
 
 def get_trainer_args():
     trainer_parser = argparse.ArgumentParser()
@@ -22,14 +31,37 @@ def get_data_args():
 
     return data_parser.parse_args()
 
+def get_config_args():
+    config_parser = argparse.ArgumentParser()
+    config_parser.add_argument("--wandb", type=bool, default=False)
+    config_parser.add_argument("--log_dir", type=str, default="logs")
+
+    return config_parser.parse_args()
+
 
 if __name__ == "__main__":
+    model_args = get_model_args()
     trainer_args = get_trainer_args()
     data_args = get_data_args()
+    config_args = get_config_args()
+
+    logger = None
+    if config_args.wandb:
+        if not os.path.exists(config_args.log_dir):
+            os.makedirs(config_args.log_dir)
+
+        logger = WandbLogger(
+            project="vision_group",
+            name=f"base_{model_args.lr}_{data_args.batch_size}",
+            save_dir=config_args.log_dir
+        )
 
     data_module = DataModule(**vars(data_args))
 
-    wrapper_model = WrapperModel(num_class=len(data_module.train.classes))
+    wrapper_model = WrapperModel(
+        num_class=len(data_module.train.classes),
+        lr=model_args.lr,
+    )
     wrapper_model.model = torch.compile(wrapper_model.model)
 
     trainer = Trainer(**vars(trainer_args))
--- model/wrapperModel.py
+++ model/wrapperModel.py
@@ -2,15 +2,31 @@ from typing import Any
 import lightning.pytorch as pl
 import torch
 import torch.nn.functional as F
+from torchmetrics import Accuracy
+
 from model.model import S3D
-from utils.utils import load_parameters
+from utils.utils import load_parameters, get_class_name
 
 
 class WrapperModel(pl.LightningModule):
-    def __init__(self, num_class: int):
+    def __init__(self, num_class: int, lr: float = 1e-03):
         super().__init__()
+
+        self.train_accuracy = Accuracy(task="multiclass", num_classes=num_class)
+        self.top_5_train_accuracy = Accuracy(task="multiclass", num_classes=num_class, top_k=5)
+        self.val_accuracy = Accuracy(task="multiclass", num_classes=num_class)
+        self.top_5_val_accuracy = Accuracy(task="multiclass", num_classes=num_class, top_k=5)
+
+        # to calculate accuracy for each class in validation
+        self.accuracies = [Accuracy(task="multiclass", num_classes=num_class) for _ in range(num_class)]
+        self.all_preds = []
+        self.all_labels = []
+
         self.model = S3D(num_class=num_class)
+
         load_parameters('./model/S3D_kinetics400.pt', self.model)
+
+        self.save_hyperparameters()
 
     def forward(self, x):
         return self.model(x)
@@ -19,17 +35,46 @@ class WrapperModel(pl.LightningModule):
         x, _, y = batch
         y_hat = self.model(x)
         loss = F.cross_entropy(y_hat, y)
+
+        # log train loss
+        self.log('train_loss', loss)
+        self.log('train_acc', self.train_accuracy(y_hat, y), on_step=True, on_epoch=True, sync_dist=True)
+        self.log('train_acc_top_5', self.top_5_train_accuracy(y_hat, y), on_step=True, on_epoch=True, sync_dist=True)
+
         return loss
 
     def validation_step(self, batch, batch_idx):
         x, _, y = batch
         y_hat = self.model(x)
         loss = F.cross_entropy(y_hat, y)
+
         # log val loss
-        # accuracy
+        self.log('val_loss', loss)
+        self.log('val_acc', self.val_accuracy(y_hat, y), on_step=True, on_epoch=True, sync_dist=True)
+        self.log('val_acc_top_5', self.top_5_val_accuracy(y_hat, y), on_step=True, on_epoch=True, sync_dist=True)
+
+        # to calculate accuracy for each class in validation
+        self.all_preds.append(torch.softmax(y_hat, dim=1))
+        self.all_labels.append(y)
+
         return loss
 
+    def on_validation_epoch_end(self):
+        all_preds = torch.cat(self.all_preds, dim=0)
+        all_labels = torch.cat(self.all_labels, dim=0)
+
+        acc_per_class = []
+        for i in range(self.hparams.num_class):
+            class_mask = (all_labels == i)
+            class_preds = all_preds[class_mask]
+            class_labels = all_labels[class_mask]
+            acc_per_class.append(self.accuracies[i](class_preds, class_labels))
+
+        self.log_dict({f"val_acc_{get_class_name(i)}": acc.item() for i, acc in enumerate(acc_per_class)})
+        self.all_preds = []
+        self.all_labels = []
+
     def configure_optimizers(self) -> Any:
-        optim = torch.optim.AdamW(self.model.parameters(), lr=1e-03)
+        optim = torch.optim.AdamW(self.model.parameters(), lr=self.hparams.lr)
         return optim
 
